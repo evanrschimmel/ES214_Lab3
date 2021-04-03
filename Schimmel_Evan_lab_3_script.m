1bee7419a385be6c33bc0c1eb6bf92497da1e596
Evan Schimmel <evanrschimmel@gmail.com>
2021-04-03 14:23:23 -0400

    Adding length of link OD into script
    Non-Grashof theta calculations still seem to be wrong

--- Schimmel_Evan_lab_3_script.m
+++ Schimmel_Evan_lab_3_script.m
@@ -8,6 +8,8 @@ Lab=240;
 Link(2)=Lab;
 Lbd=165;
 Link(3)=Lbd;
+Lod=190;
+Link(4)=Lod;
 
 if sum(Link) >= 2*(min(Link)+max(Link))
     fprintf('System is Grashof\n');
@@ -15,5 +17,6 @@ if sum(Link) >= 2*(min(Link)+max(Link))
     ThetaOAmin=0;
 elseif sum(Link) < 2*(min(Link)+max(Link))
     fprintf('System is Non-Grashof\n');
-    ThetaOAmin=
+    ThetaOAmax=acos(((Loa^2)+(Lod^2)-((Lab-Lbd)^2))/(2*Loa*Lod));
+    ThetaOAmin=-ThetaOAmax;
 end
