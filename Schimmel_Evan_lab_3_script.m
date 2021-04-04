9abeac62c986d6802b12660e6e83749a9bd2019b
Evan Schimmel <evanrschimmel@gmail.com>
2021-04-03 22:23:50 -0400

    Degrees are a killer

--- Schimmel_Evan_lab_3_script.m
+++ Schimmel_Evan_lab_3_script.m
@@ -19,7 +19,7 @@ if sum(Link) >= 2*(min(Link)+max(Link))
     ThetaOAmin=0;
 elseif sum(Link) < 2*(min(Link)+max(Link))
     fprintf('System is Non-Grashof\n');
-    ThetaOAmax=acos(((Loa^2)+(Lod^2)-((Lab-Lbd)^2))/(2*Loa*Lod));
+    ThetaOAmax=acosd(((Loa^2)+(Lod^2)-((Lab-Lbd)^2))/(2*Loa*Lod));
     ThetaOAmin=-ThetaOAmax;
 end
 
@@ -27,21 +27,24 @@ i=0;
 for angle=ThetaOAmin:1:ThetaOAmax
     i=i+1;
     Lad=sqrt((Loa^2)+(Lod^2)-(2*Loa*Lod*cosd(angle)));
-    alpha=asin((Loa/Lad)*sind(angle));
-    beta=acos(((Lab^2)+(Lbd^2)-(Lad^2))/(2*Lab*Lbd));
-    gamma=asin((Lbd/Lad)*sind(beta));
+    alpha=asind((Loa/Lad)*sind(angle));
+    beta=acosd(((Lab^2)+(Lbd^2)-(Lad^2))/(2*Lab*Lbd));
+    gamma=asind((Lbd/Lad)*sind(beta));
     epsilon=180-beta-gamma;
     ThetaBD=180-alpha-epsilon;
     ThetaAB=ThetaBD-beta;
     Xa(i)=Loa*cosd(angle);
     Ya(i)=Loa*sind(angle);
     Xb(i)=Lod+Lbd*cosd(ThetaBD);
-    Yb(i)=Lod+Lbd*sind(ThetaBD);
+    Yb(i)=Lbd*sind(ThetaBD);
     Xc(i)=Loa*cosd(angle)+Lac*cosd(phi+ThetaAB);
     Yc(i)=Loa*sind(angle)+Lac*sind(phi+ThetaAB);
 end
 
-%axis equal
-%plot(Xa,Ya,Xb,Yb,Xc,Yc);
+plot(Xa,Ya,Xb,Yb,Xc,Yc,);
+axis equal
+legend('Point A','Point B','Point C');
+xlabel('X Position [mm]');
+ylabel('Y Position [mm]');
 
 animate_four_bar(Xa,Ya,Xb,Yb,Xc,Yc,Lod);
